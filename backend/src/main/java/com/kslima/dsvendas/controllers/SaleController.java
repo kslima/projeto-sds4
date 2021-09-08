7784c6e53600b650f26e74d9e620ae3523af54a0
Kleuder Lima <kleuder.lima@gmail.com>
2021-09-08 20:24:52 -0300

    Group by search

--- backend/src/main/java/com/kslima/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/kslima/dsvendas/controllers/SaleController.java
@@ -1,6 +1,8 @@
 package com.kslima.dsvendas.controllers;
 
 import com.kslima.dsvendas.dto.SaleDTO;
+import com.kslima.dsvendas.dto.SaleSuccessDTO;
+import com.kslima.dsvendas.dto.SaleSumDTO;
 import com.kslima.dsvendas.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -22,6 +24,18 @@ public class SaleController {
     @GetMapping
     public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
         Page<SaleDTO> sales = saleService.findAll(pageable);
+        return ResponseEntity.ok(sales);
+    }
+
+    @GetMapping(value = "/amount-by-seller")
+    public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller() {
+        List<SaleSumDTO> sales = saleService.amountGroupedBySeller();
+        return ResponseEntity.ok(sales);
+    }
+
+    @GetMapping(value = "/success-by-seller")
+    public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller() {
+        List<SaleSuccessDTO> sales = saleService.successGroupedBySeller();
         return ResponseEntity.ok(sales);
     }
 }
--- backend/src/main/java/com/kslima/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/kslima/dsvendas/services/SaleService.java
@@ -1,6 +1,8 @@
 package com.kslima.dsvendas.services;
 
 import com.kslima.dsvendas.dto.SaleDTO;
+import com.kslima.dsvendas.dto.SaleSuccessDTO;
+import com.kslima.dsvendas.dto.SaleSumDTO;
 import com.kslima.dsvendas.entities.Sale;
 import com.kslima.dsvendas.repositories.SaleRepository;
 import com.kslima.dsvendas.repositories.SellerRepository;
@@ -9,6 +11,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
@@ -25,5 +29,15 @@ public class SaleService {
         sellerRepository.findAll();
         Page<Sale> result = saleRepository.findAll(pageable);
         return result.map(SaleDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public List<SaleSumDTO> amountGroupedBySeller() {
+        return saleRepository.amountGroupedBySeller();
+    }
+
+    @Transactional(readOnly = true)
+    public List<SaleSuccessDTO> successGroupedBySeller() {
+        return saleRepository.successGroupedBySeller();
     }
 }
